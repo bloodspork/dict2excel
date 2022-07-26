40551e47b26457553971019f63a069977d7cc47c
bloodspork <bloodspork@tutanota.com>
2022-07-26 15:50:46 -0700

    refinement; reasonable column widths

--- dict2excel.py
+++ dict2excel.py
@@ -3,26 +3,40 @@
 
 from openpyxl import Workbook
 
+def autoFitColumnWidth(ws):
+    dims = {}
+    for row in ws.rows:
+        for cell in row:
+            if cell.value:
+                dims[cell.column_letter] = \
+                    max((dims.get(cell.column_letter, 0), len(str(cell.value))))
+    for col, value in dims.items():
+        ws.column_dimensions[col].width = value + 4
+
+
 def dict2excel(dct: dict, filepath: str):
     wb = Workbook()
     for sheetname in dct:
         sheet = wb.create_sheet(sheetname)
         row_list = dct[sheetname]
 
-        fieldname_pos_dict = {}
         colnum = 1
         rownum = 1
         for row in row_list:
             if rownum == 1:
                 for fieldname in row:
                     sheet.cell(rownum, colnum, fieldname)
-                    fieldname_pos_dict['fieldname'] = colnum
                     colnum += 1
                 rownum += 1
-            
+
+            colnum = 1
             for fieldname in row:
                 sheet.cell(rownum, colnum, row[fieldname])
+                colnum += 1
             rownum += 1
+        autoFitColumnWidth(sheet)
 
+    del wb['Sheet']
     wb.save(filepath)
+
 
